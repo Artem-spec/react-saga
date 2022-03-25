6470c29eb7999fb4898a807fe894a9bcb01089a3
Гринблат Артём Андреевич <artem.grinblat@OCRV.RU>
2022-03-25 20:33:47 +0400

    Промежуточные изменения playwright

--- src/components/cats/catSaga.ts
+++ src/components/cats/catSaga.ts
@@ -3,10 +3,8 @@ import {getCatsSuccess} from './catState'
 
 
 function* workGetCatsFetch(){
-    const cats = yield call(()=> fetch('https://api.thecatapi.com/v1/breeds'))  
-    const formattedCats = yield cats.json();
-    const formattedCatsShortened = formattedCats.slice(0, 10)
-    yield put(getCatsSuccess(formattedCatsShortened))
+    const cats: Promise<any> = yield call(()=> fetch('https://api.thecatapi.com/v1/breeds'))  
+    yield put(getCatsSuccess(cats))
 }
 
 function* catSaga(){
